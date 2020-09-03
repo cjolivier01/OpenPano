270d6281c8980456c44034405310e7fa989b4b60
Claude Pageau <pageauc@gmail.com>
2020-09-02 22:17:17 -0400

    Added Install script for eigen3

---
+++ eigen3-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "OpenPano prerequiste on some computers"
+echo "Compile and build eigen3 libraries"
+cd ~/
+echo "git clone https://github.com/libigl/eigen"
+git clone https://github.com/libigl/eigen
+cd eigen
+mkdir build
+cd build
+cmake
+make
+sudo make install
+echo "eigen3 should be installed"
+echo "Bye"
