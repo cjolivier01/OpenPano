6f685c3c3177b0cf02d228360df2ab959eb0b7c7
Chris Olivier <cjolivier01@apache.org>
2023-07-11 12:18:36 -0700

    test

--- test-pano.sh
+++ test-pano.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+set -e
 echo "Testing image-stitching"
-cd ~/OpenPano
+#cd ~/OpenPano
 echo "Running"
 echo "./image-stitching ./images/test-pano.jpg ./images/img1.jpg ./images/img2.jpg ./images/img3.jpg"
-./image-stitching ./images/test-pano.jpg ./images/img1.jpg ./images/img2.jpg ./images/img3.jpg
+./image-stitching ./images/test-pano.jpg ./images/img1.jpg ./images/img2.jpg
 echo "Final stitched image test-pano.jpg will be in images folder"
 ls images
 echo "Exit Test"
