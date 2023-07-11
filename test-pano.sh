baa042ae413de1860b618fa42042524f5e76e92f
Chris Olivier <cjolivier01@apache.org>
2023-07-11 12:21:35 -0700

    Three test images, ignore output

--- test-pano.sh
+++ test-pano.sh
@@ -4,7 +4,7 @@ echo "Testing image-stitching"
 #cd ~/OpenPano
 echo "Running"
 echo "./image-stitching ./images/test-pano.jpg ./images/img1.jpg ./images/img2.jpg ./images/img3.jpg"
-./image-stitching ./images/test-pano.jpg ./images/img1.jpg ./images/img2.jpg
+./image-stitching ./images/test-pano.jpg ./images/img1.jpg ./images/img2.jpg ./images/img3.jpg
 echo "Final stitched image test-pano.jpg will be in images folder"
 ls images
 echo "Exit Test"
